885e9f97746abcccaea06adc1ffa9311cad5938e
Yongha Hwang <yongha.hwang@gmail.com>
2020-01-10 15:06:53 -0500

    tippecanoe script added

---
+++ geojson2pbf.sh
@@ -0,0 +1,5 @@
+docker run -it \
+-v ~/data/geojson:/tmp yonghah/tippecanoe-ubuntu:latest \
+tippecanoe \ 
+/tmp/building.geojson /tmp/floor.geojson /tmp/room.geojson /tmp/reference.geojson \
+-e /tmp/tile -z18 -pf -pk -f -pC
